deeae3e660edb443221852542ef12f9ae976f779
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2026-07-09 20:25:41 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 py -3.12 -m venv x
 call x\Scripts\activate.bat
-py -3.12 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 echo "Instalasi selesai. Jalankan main.py untuk memulai aplikasi."
 pause
